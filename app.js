19e8f8313d193a39e75b0fb7075dbef9f63df4c2
Ritam Majumdar <149047274+ritam1105@users.noreply.github.com>
2026-06-12 20:33:47 +0530

    commit

--- app.js
+++ app.js
@@ -40,4 +40,8 @@ app.get('/graph/:sessionId/:sensor', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server running at http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
